c6b3d26dc70421de52dd76cbe091f5b79f0b67b1
Victor Girao <victorgiraoc@hotmail.com>
2021-09-10 09:47:35 -0300

    mudança de db e criação de chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,5 @@
 class ChatsController < ApplicationController
   def show
+    @messages = Message.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: "chats#show"
   get 'chats/show'
   resources :users
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
